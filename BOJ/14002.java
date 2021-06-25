2c37ebb9ec88e77263d8a91dc22b369f2e6a7556
Jong Seo Ha <hajongse@naver.com>
2021-06-25 15:57:44 +0900

    BOJ-14002

---
+++ BOJ/14002.java
@@ -0,0 +1,46 @@
+//14002 - 가장 긴 증가하는 부분 수열4
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] inp = new int[n+1];
+		int[] dp = new int[n+1];
+		ArrayList<Integer> lis = new ArrayList<Integer>();
+		
+		for (int i=1; i<=n; i++)
+			inp[i] = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			int max = Integer.MIN_VALUE;
+			for (int j = 0; j <= i; j++) {
+				if (inp[i] > inp[j]) {
+					if (max < dp[j] + 1) {
+						max = dp[j] + 1;
+					}
+				}
+			}
+			dp[i] = max;
+		}
+		int max = Integer.MIN_VALUE;
+		for (int i=1; i<=n; i++) {
+			if (max < dp[i])
+				max = dp[i];
+		}
+		System.out.println(max);
+		for (int i=n; i>0; i--) {
+			if (dp[i] == max) {
+				lis.add(inp[i]);
+				max--;
+			}
+		}
+		for (int i=lis.size()-1; i>=0; i--)
+			System.out.print(lis.get(i)+" ");
+		sc.close();
+	}
+}
