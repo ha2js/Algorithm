07004c0a2875c6a4d39605d89dcebdd355fa500a
Jong Seo Ha <hajongse@naver.com>
2021-07-15 13:53:49 +0900

    BOJ-1158

---
+++ BOJ/1158.java
@@ -0,0 +1,33 @@
+// 1158 - 요세푸스 문제
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String bfstr = bf.readLine();
+		StringTokenizer st = new StringTokenizer(bfstr);
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int cnt = n;
+		
+		Queue<Integer> que = new LinkedList<Integer>();
+		for (int i=1; i<=n; i++) que.offer(i);
+		
+		System.out.print("<");
+		while (cnt > 0) {
+			if (cnt == 1) System.out.print(que.poll());
+			else {
+				for (int i=0; i<k-1; i++)
+					que.offer(que.poll());
+				System.out.print(que.poll()+", ");
+			}
+			cnt--;
+		}
+		System.out.println(">");
+		bf.close();
+	}
+}
