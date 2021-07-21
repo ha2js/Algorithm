fb5aec108064e4d63a703f29461fc250e5849d28
Jong Seo Ha <hajongse@naver.com>
2021-07-21 11:21:36 +0900

    BOJ - 2164

---
+++ BOJ/2164.java
@@ -0,0 +1,27 @@
+// 2164 - 카드2
+import java.io.*;
+import java.util.*;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bf.readLine());
+		Deque<Integer> queue = new ArrayDeque<>();
+		
+		for (int i=1; i<=n; i++)
+			queue.add(i);
+		
+		while (true) {
+			if (queue.size() == 1) break;
+			queue.removeFirst();
+			queue.addLast(queue.getFirst());
+			queue.removeFirst();
+		}
+		System.out.println(queue.getFirst());
+		bf.close();
+	}
+}
