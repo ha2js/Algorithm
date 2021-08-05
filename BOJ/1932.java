3c550dc93cde07a3f323f2f9f392a7d1bb6eaa55
Jong Seo Ha <hajongse@naver.com>
2021-08-05 12:24:39 +0900

    BOJ-1932

--- BOJ/1932.java
+++ BOJ/1932.java
@@ -1,4 +1,4 @@
-// 1932 - 정수 삼각형
+//1932 - 정수 삼각형
 import java.util.*;
 import java.io.*;
 
@@ -8,23 +8,23 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int[][] inp = new int[n][n];
 		int[][] dp = new int[n][n];
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<=i; j++) inp[i][j] = sc.nextInt();
-		}
-		dp[1][0] = inp[0][0] + inp[1][0];
-		dp[1][1] = inp[0][0] + inp[1][1];
 		
-		for (int i=2; i<n; i++) {
-			for (int j=0; j<n; j++) {
-				if (j==0) dp[i][j] = dp[i-1][j] + inp[i][j];
-				else dp[i][j] = Math.max(dp[i-1][j]+inp[i][j],dp[i-1][j-1]+inp[i][j]);
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<=i; j++) 
+				dp[i][j] = sc.nextInt();
+		}
+
+		for (int i=1; i<n; i++) {
+			for (int j=0; j<=i; j++) {
+				if (j-1>=0) dp[i][j] += Math.max(dp[i-1][j], dp[i-1][j-1]);
+				else dp[i][j] += dp[i-1][j];
 			}
 		}
 		int result = Integer.MIN_VALUE;
 		for (int i=0; i<n; i++) {
-			if (result < dp[n-1][i]) result = dp[n-1][i];
+			if (result < dp[n-1][i]) 
+				result = dp[n-1][i];
 		}
 		System.out.println(result);
 		sc.close();
