d7c55065ca001f5827de7dde26f35f658e931a18
Jong Seo Ha <hajongse@naver.com>
2021-08-11 11:16:52 +0900

    BOJ-10814

---
+++ BOJ/10814.java
@@ -0,0 +1,34 @@
+// 10814 - 나이순 정렬
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		String[][] arr = new String[n][2];
+		
+		StringTokenizer st = null;
+		
+		for (int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
+		}
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.compare(Integer.parseInt(s1[0]), Integer.parseInt(s2[0]));
+			}
+		});
+
+		for (int i=0; i<n; i++)
+			System.out.println(arr[i][0]+" "+arr[i][1]);
+		
+		br.close();	
+	}
+}
