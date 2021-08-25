8bb0c5b03e1e45b5984ef13a9652835dd5e216db
Jong Seo Ha <hajongse@naver.com>
2021-08-25 09:33:32 +0900

    BOJ-1475

---
+++ BOJ/1475.java
@@ -0,0 +1,35 @@
+// 1475 - 방 번호 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		int[] num = new int[9];
+		
+		for (int i=0; i<n.length(); i++) {
+			int tmp = n.charAt(i) - '0';
+			
+			if (tmp == 6 || tmp == 9) num[6]++;
+			else num[tmp]++;
+		}
+		
+		if (num[6] % 2 == 1) {
+			num[6] /= 2;
+			num[6]++;
+		}
+		else num[6] /= 2;
+
+		int result = Integer.MIN_VALUE;
+		for (int i=0; i<9; i++) {
+			result = Math.max(result, num[i]);
+		}
+		
+		System.out.println(result);
+		br.close();	
+	}
+}
