01e8ebab43ae630531173b4a29e8e7de2e82e3b7
Jong Seo Ha <hajongse@naver.com>
2021-08-26 12:22:26 +0900

    BOJ | 10799 | 쇠막대기

---
+++ BOJ/10799_쇠막대기/BOJ_10799_쇠막대기.java
@@ -0,0 +1,32 @@
+// 10799 - 쇠막대기
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String stick = br.readLine();
+		
+		int open = 0,close = 0,result = 0;
+		
+		for (int i=0; i<stick.length(); i++) {
+			if (stick.charAt(i) == '(' && stick.charAt(i+1) == ')') {
+				result += open + close;
+				close = 0;
+				i++;
+			}
+			else if (stick.charAt(i) == '(') {
+				open++;
+			}
+			else if (stick.charAt(i) == ')') {
+				open--;
+				close++;
+			}
+		}
+		System.out.println(result+open+close);
+		br.close();	
+	}
+}
